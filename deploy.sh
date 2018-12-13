5322c6419ada18334d86418c2bcf9c64b6391c98
Jindrich Mynarz <mynarzjindrich@gmail.com>
2018-12-13 22:13:49 +0100

    Rudimentary deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ex
+
+git checkout gh-pages
+
+# Build Clojurescript
+lein cljsbuild once min
+
+# Copy built files into root
+cp resources/public/index.html .
+mkdir -p js/compiled
+cp resources/public/js/compiled/app.js js/compiled
+cp -R resources/public/css .
+cp -R resources/public/vendor .
+
+git add index.html js css vendor
+git commit -m "Deploy"
+git push
